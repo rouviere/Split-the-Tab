2763933c764d15cebfa0569db2a7871adf8fb9b1
Forrest Anderson <forrest@rouviere.com>
2019-11-12 16:34:38 -0700

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Forrest Anderson on 11/12/19.
+//  Copyright © 2019 Forrest Anderson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
